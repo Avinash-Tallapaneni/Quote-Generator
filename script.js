c7e569d0d11282df288e5e1ecfead844c622cb02
Avinash-Tallapaneni <104318799+Avinash-Tallapaneni@users.noreply.github.com>
2022-12-22 20:01:14 +0530

    feat : quote can be tweeted/copy/post. Changed icons from font awesome to SVG

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let quote = document.getElementById("quote");
-let author = document.getElementById("author");
-let newQuote = document.getElementById("new-quote");
+let quote = document.querySelector("#quote");
+let author = document.querySelector("#author");
+let newQuote = document.querySelector("#new-quote");
+let twitterBtn = document.querySelector("#twitter");
+let copyBtn = document.querySelector("#copy");
 
 
 // const url = "https://api.quotable.io/random";
@@ -10,14 +12,35 @@ let getQuote = () => {
     fetch(url)
         .then(response => response.json())
         .then(response =>  {
-            const firstObject = response[Math.floor(Math.random()*1500)];
+            const firstObject = response[Math.floor(Math.random()*response.length)];
             console.log(firstObject.text);
             console.log(firstObject.author);
             quote.innerText = firstObject.text;
-            author.innerText = firstObject.author;
+         if(!firstObject.author) {
+                    author.innerText = "- Anonymous";
+                    
+            } else author.innerText = "- " + firstObject.author;
+            
+        
         });
 }
 
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quote.innerText} ${author.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+
+copyBtn.addEventListener("click", () => {
+    // console.log("button was clicked");     
+    navigator.clipboard
+      .writeText(quote.innerText)
+      .then(
+              success => console.log("text copied"), 
+              err => console.log("error copying text")
+        );
+});
+    
+    
 
 window.addEventListener("load", getQuote);
 newQuote.addEventListener("click", getQuote);
